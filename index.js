9bbc21433cecc6c91cb12fde274e8569c0b5d13e
NAZARPC\hopest <hopest7@gmail.com>
2017-06-23 00:07:17 +0300

    b2

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var app = express();
 
+//Load modules
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('BHSEk.SQLite3');
+
+//Perform SELECT Operation
+db.all("SELECT * FROM books", function (err, rows) {
+    console.log(rows);
+    //rows contain values while errors, well you can figure out.
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
- app.set('views', __dirname + '/views');
- app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
+app.get('/', function (request, response) {
+    response.render('pages/index')
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 })
