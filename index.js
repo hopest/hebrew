1ac1513c00f8e7cf86a20f13b7a8c0b55da5f6f1
NAZARPC\hopest <hopest7@gmail.com>
2017-06-24 16:46:09 +0300

    template

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var app = express();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('BHSEk.SQLite3');
 
-//Perform SELECT Operation
-db.all("SELECT * FROM books", function (err, rows) {
-    console.log(rows);
-    //rows contain values while errors, well you can figure out.
-});
+// //Perform SELECT Operation
+// db.all("SELECT * FROM books", function (err, rows) {
+//     console.log(rows);
+//     //rows contain values while errors, well you can figure out.
+// });
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -19,8 +19,37 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function (request, response) {
-    response.render('pages/index')
+app.get('/', function (req, res, next) {
+
+    db.all('SELECT book_number, chapter, verse FROM verses WHERE verses.book_number=10 AND verses.chapter = 1', function (err, row) {
+        if (err !== null) {
+      
+            next(err);
+        } else {
+            console.log(row);
+            res.render('pages/index', {
+                bookmarks: row,
+                title: "Мои контакты",
+                emailsVisible: true,
+                emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+                phone: "+1234567890"
+
+            }, function (err, html) {
+                res.send(200, html);
+            });
+        }
+
+        // res.render('pages/index', {
+        //     title: "Мои контакты",
+        //     emailsVisible: true,
+        //     emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+        //     phone: "+1234567890"
+        // })
+
+    });
+
+
+
 });
 
 app.listen(app.get('port'), function () {
